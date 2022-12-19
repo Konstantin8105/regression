aba46fedcd9a5dc9972c2aa6e1b66a7c58a08e8b
Konstantin8105 <konstantin8105@gmail.com>
2022-12-19 22:13:57 +0300

    linear regression

--- README.md
+++ README.md
@@ -8,6 +8,16 @@ package regression // import "github.com/Konstantin8105/regression"
 
 FUNCTIONS
 
+func Linear(data [][2]float64) (
+	a, b float64,
+	R2 float64,
+	err error,
+)
+    Linear regression model:
+
+        y   = a*x+b
+        R^2 - the relative predictive power of a linear model
+
 func Quadratic(data [][2]float64) (
 	a, b, c float64,
 	R2 float64,
--- regression_test.go
+++ regression_test.go
@@ -6,6 +6,23 @@ import (
 	"testing"
 )
 
+func ExampleLinear() {
+	a, b, R2, err := Linear([][2]float64{
+		{2, 3},
+		{4, 7},
+		{6, 5},
+		{8, 10},
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(os.Stdout, "y   = %.4f*x+%.4f\n", a, b)
+	fmt.Fprintf(os.Stdout, "R^2 = %.4f\n", R2)
+	// Output:
+	// y   = 1.5000*x+0.9500
+	// R^2 = 0.6748
+}
+
 func ExampleQuadratic() {
 	a, b, c, R2, err := Quadratic([][2]float64{
 		{-3.00, 7.50},
@@ -22,8 +39,8 @@ func ExampleQuadratic() {
 	fmt.Fprintf(os.Stdout, "y   = %.4f*x^2+%.4f*x+%.4f\n", a, b, c)
 	fmt.Fprintf(os.Stdout, "R^2 = %.4f\n", R2)
 	// Output:
-	// y   = 1.1071*x^2+1.0000*x+0.5714
-	// R^2 = 0.9884
+	// y   = 0.5714*x^2+1.0000*x+1.1071
+	// R^2 = 0.6700
 }
 
 func TestQuadratic2points(t *testing.T) {
